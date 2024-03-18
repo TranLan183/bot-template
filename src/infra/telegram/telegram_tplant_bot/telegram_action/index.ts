c2c1d24905cd6e189b14383edc11f9c808066084
PhucLan <lantp@theparallel.io>
2024-03-18 16:02:34 +0700

    Update cache

--- src/infra/telegram/telegram_tplant_bot/telegram_action/index.ts
+++ src/infra/telegram/telegram_tplant_bot/telegram_action/index.ts
@@ -4,7 +4,7 @@ import { ErrorHandler } from "../../../../lib/error_handler"
 import { ConvertTeleError } from "../../telegram.error"
 import { convertActionContext } from "../../telegram.lib"
 import { handleInvalidCacheUserStorage } from "../helper_bot"
-import { getUserStorage } from "../telegram_cache/cache.data_user"
+import { getDataUserCache } from "../telegram_cache/cache.data_user"
 
 const methodAction = {
 }
@@ -13,7 +13,7 @@ export const handleBotAction = () => {
     tele_bot.on('callback_query', async (ctx) => {
         const { callbackData, userId, callbackId, gameInfo, gameShortName, username } = convertActionContext(ctx);
         try {
-            let dataUserStorage = await getUserStorage(userId)
+            let dataUserStorage = await getDataUserCache(userId)
             if (!dataUserStorage) dataUserStorage = await handleInvalidCacheUserStorage(userId, username)
             if (gameInfo && gameShortName) {
                 await ctx.telegram.answerCbQuery(callbackId, undefined, {
