6134dc4a12b8395d7b06bd6a97d6705a5f2a7ab2
PhucLan <lantp@theparallel.io>
2025-04-04 11:45:32 +0700

    Remove file test

--- src/test/telegram.test.ts
+++
@@ -1,8 +0,0 @@
-import { initRedis } from '../infra/cache/redis';
-import { bot_example } from '../infra/telegram/telegram_bot/index';
-
-const test = async () => {
-    await initRedis()
-    bot_example.init()
-}
-test()
