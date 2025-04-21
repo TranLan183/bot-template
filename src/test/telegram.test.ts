ed2463166bb9ed4fc170dca745443181b2460d20
PhucLan <lantp@theparallel.io>
2025-04-21 10:12:23 +0700

    Update .gitignore to add the test/ directory and remove the unused telegram.test.ts file.

--- src/test/telegram.test.ts
+++
@@ -1,8 +0,0 @@
-import { initRedis } from '../infra/cache/redis';
-import { bot_template } from '../infra/telegram/telegram_bot/index';
-
-const test = async () => {
-    await initRedis()
-    bot_template.init()
-}
-test()
